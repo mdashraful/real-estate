1f60fbf47cf17bd07912967c5e2282d124bb3883
Md.Ashraful Islam <mdashrafulislam369@gmail.com>
2026-01-06 16:47:12 +0600

    registration and login form design

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\FrontController;
+use App\Http\Controllers\RegisterUserController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['as' => 'front.'], function () {
+    Route::get('/', [FrontController::class, 'home'])->name('home');
+    Route::get('/about', [FrontController::class, 'about'])->name('about');
 });
+
+Route::get('/register', [RegisterUserController::class, 'create'])->name('register');
+Route::get('/login', [SessionController::class, 'create'])->name('login');
